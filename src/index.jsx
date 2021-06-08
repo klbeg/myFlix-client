66999a976ad7beb4b606387432e4e88df3c97b39
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-06-07 20:18:57 -0700

    main-view.jsx: -created, contains code for 'main-view' index.jsx: - imports MainView and renders it in MyFlixApplication

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view.jsx';
 
 //  Import statement to indicate that you need to bundle
 //  './index.scss'
@@ -8,11 +9,7 @@ import './index.scss';
 //  Main component (will eventually use all others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
-    );
+    return <MainView />;
   }
 }
 
