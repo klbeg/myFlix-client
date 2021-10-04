a0465fdee30b34fa57dd7c896d4c34d58d3f5aa8
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-10-04 13:26:03 -0400

    Revert "Merge branch 'fixing-registration'"
    This reverts commit 16b391188e20f9ce21cd85ea1bf168814b438dfc, reversing
    changes made to a7181d12d56d5601608532a4356c12c0ddfd70be.

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -12,181 +12,168 @@ import Button from 'react-bootstrap/Button';
 import { host } from '../../config';
 
 import './registration-view.scss';
-import { Card, Row } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
-class RegistrationView extends Component {
-  constructor() {
-    super();
-    const onBackClick = this.props;
-    let updateToggle = false;
-    this.handleUserInput = this.handleUserInput.bind(this);
-  }
+export function RegistrationView(props) {
+  const { onBackClick } = props;
 
   //  Input validation for registration form
-  handleUserInput = (evt) => {
-    this.props.setNewUser({
+  const handleUserInput = (evt) => {
+    props.setNewUser({
       [evt.target.name]: evt.target.value,
     });
   };
 
-  userUpdateValidation = () => {
+  const userUpdateValidation = () => {
     console.log('userUpdateValidation being called');
 
-    userUpdateValidation = () => {
-      let isValid = true;
-      let errors = this.props.errors;
-      let name = this.props.newUser.Name;
-      let username = this.props.newUser.Username;
-      let password = this.props.newUser.Password;
-      let email = this.props.newUser.Email;
-      let birthdate = this.props.newUser.Birthdate;
+    let isValid = true;
+    const errors = {};
+    let name = props.newUser.Name;
+    let username = props.newUser.Username;
+    let password = props.newUser.Password;
+    let email = props.newUser.Email;
+    let birthdate = props.newUser.Birthdate;
 
-      if (!name) {
-        errors.nameIsRequired = 'Name is a required field';
+    if (!name) {
+      errors.nameIsRequired = 'Name is a required field';
+      isValid = false;
+    }
+    if (!/^[a-z ]*$/i.test(name)) {
+      errors.nameContent = 'Name may contain letters only.';
+      isValid = false;
+    }
+    if (!username) {
+      errors.usernameIsRequired = 'Username is a required field.';
+      isValid = false;
+    } else {
+      if (username.length <= 5) {
+        errors.usernameLength = 'Username must be 5 or more characters.';
         isValid = false;
       }
-      if (!/^[a-z ]*$/i.test(name)) {
-        errors.nameContent = 'Name may contain letters only.';
+      if (!/^[a-z0-9]+$/i.test(username)) {
+        errors.usernameContent =
+          'Username may only contain alphanumeric characters.';
         isValid = false;
       }
-      if (!username) {
-        errors.usernameIsRequired = 'Username is a required field.';
-        isValid = false;
-      } else {
-        if (username.length <= 5) {
-          errors.usernameLength = 'Username must be 5 or more characters.';
-          isValid = false;
-        }
-        if (!/^[a-z0-9]+$/i.test(username)) {
-          errors.usernameContent =
-            'Username may only contain alphanumeric characters.';
-          isValid = false;
-        }
-      }
-      if (!password) {
-        errors.passwordIsRequired = 'Password is a required field.';
-        isValid = false;
-      } else {
-        if (password.length < 7) {
-          errors.passwordLength = 'Passwords must be 7 or more characters.';
-          isValid = false;
-        }
-      }
-      if (!email) {
-        errors.emailIsRequired = 'Email is a required field.';
-        isValid = false;
-      } else {
-        if (!/[@]/g.test(email)) {
-          errors.emailNotValid = 'Email must be valid email.';
-          isValid = false;
-        }
-      }
-      if (!birthdate) {
-        errors.birthdateIsRequired = 'Birthdate is a required field.';
+    }
+    if (!password) {
+      errors.passwordIsRequired = 'Password is a required field.';
+      isValid = false;
+    } else {
+      if (password.length < 7) {
+        errors.passwordLength = 'Passwords must be 7 or more characters.';
         isValid = false;
       }
+    }
+    if (!email) {
+      errors.emailIsRequired = 'Email is a required field.';
+      isValid = false;
+    } else {
+      if (!/[@]/g.test(email)) {
+        errors.emailNotValid = 'Email must be valid email.';
+        isValid = false;
+      }
+    }
+    if (!birthdate) {
+      errors.birthdateIsRequired = 'Birthdate is a required field.';
+      isValid = false;
+    }
 
-      this.props.setErrors(errors);
-      return isValid;
-    };
-
-    handleSubmit = (e) => {
-      e.preventDefault();
-      const isValid = this.userUpdateValidation();
-      this.setState({
-        updateToggle: true,
-      });
-
-      if (isValid) {
-        axios
-          .post(host + '/users', {
-            Name: this.props.newUser.Name,
-            Username: this.props.newUser.Username,
-            Password: this.props.newUser.Password,
-            Email: this.props.newUser.Email,
-            Birthdate: this.props.newUser.Birthdate,
-          })
-          .then((response) => {
-            const data = response.data;
-            console.log(data);
-            alert(
-              `Welcome ${this.props.newUser.Username}.  Your account has been created, please log in.`
-            );
-            window.open('/', '_self');
-          })
-          .catch((e) => {
-            console.log('The following error occured: ' + e);
-          });
-      }
-      this.setState({
-        updateToggle: false,
-      });
-    };
+    props.setErrors(errors);
+    return isValid;
   };
-  render() {
-    return (
-      <Card.Body className="registration-view-container basic-card-styling p-3">
-        <div className="col-md-5 col-xsm-12">
-          <Form>
-            <Form.Group controlId="formName">
-              <Form.Label>Name:</Form.Label>
-              <Form.Control
-                name="Name"
-                type="text"
-                onChange={this.handleUserInput}
-              />
-            </Form.Group>
-            <Form.Group controlId="formUsername">
-              <Form.Label>Username:</Form.Label>
-              <Form.Control
-                name="Username"
-                type="text"
-                onChange={this.handleUserInput}
-              />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password:</Form.Label>
-              <Form.Control
-                name="Password"
-                type="text"
-                onChange={this.handleUserInput}
-              />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email:</Form.Label>
-              <Form.Control
-                name="Email"
-                type="text"
-                onChange={this.handleUserInput}
-              />
-            </Form.Group>
-            <Form.Group controlId="formBirthdate">
-              <Form.Label>Birthdate</Form.Label>
-              <Form.Control
-                name="Birthdate"
-                type="date"
-                onChange={this.handleUserInput}
-              />
-            </Form.Group>
-            {Object.values(this.props.errors).map((value) => {
-              return (
-                <div className="display-errors" key={value}>
-                  {value}
-                </div>
-              );
-            })}
-            <Button type="submit" onClick={this.handleSubmit}>
-              Submit
-            </Button>
-            <Link className="btn btn-primary" to={`/`}>
-              Back
-            </Link>
-          </Form>
-        </div>
-      </Card.Body>
-    );
-  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = this.userUpdateValidation();
+
+    if (isValid) {
+      axios
+        .post(host + '/users', {
+          Name: props.newUser.Name,
+          Username: props.newUser.Username,
+          Password: props.newUser.Password,
+          Email: props.newUser.Email,
+          Birthdate: props.newUser.Birthdate,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          alert(
+            `Welcome ${props.newUser.Username}.  Your account has been created, please log in.`
+          );
+          window.open('/', '_self');
+        })
+        .catch((e) => {
+          console.log('The following error occured: ' + e);
+        });
+    }
+  };
+
+  return (
+    <Card.Body className="registration-view-container basic-card-styling p-3">
+      <div className="col-md-5 col-xsm-12">
+        <Form>
+          <Form.Group controlId="formName">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
+              name="Name"
+              type="text"
+              onChange={this.handleUserInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control
+              name="Username"
+              type="text"
+              onChange={this.handleUserInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              name="Password"
+              type="text"
+              onChange={this.handleUserInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              name="Email"
+              type="text"
+              onChange={this.handleUserInput}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBirthdate">
+            <Form.Label>Birthdate</Form.Label>
+            <Form.Control
+              name="Birthdate"
+              type="date"
+              onChange={this.handleUserInput}
+            />
+          </Form.Group>
+          {Object.values(props.errors).map((value) => {
+            return (
+              <div className="display-errors" key={value}>
+                {value}
+              </div>
+            );
+          })}
+          <Button type="submit" onClick={handleSubmit}>
+            Submit
+          </Button>
+          <Link className="btn btn-primary" to={`/`}>
+            Back
+          </Link>
+        </Form>
+      </div>
+    </Card.Body>
+  );
 }
+
 let mapStateToProps = (state) => {
   return {
     user: state.user,
